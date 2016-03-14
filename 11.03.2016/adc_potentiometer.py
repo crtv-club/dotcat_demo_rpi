540b147553943fad8de76f250c7a9382eea5929d
Sergey Kostyuk <ks.hot.ua@gmail.com>
2016-03-14 15:23:07 +0200

    Moved readadc function into separate file

--- 11.03.2016/adc_potentiometer.py
+++ 11.03.2016/adc_potentiometer.py
@@ -4,44 +4,11 @@
 import RPi.GPIO as GPIO
 import time
 
+import adc
+
 # Используем нумерацию выводов по типу GPIO.BOARD,
 # подробнее: http://bit.ly/1pC3VEs
 GPIO.setmode(GPIO.BOARD)
-
-# read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
-def readadc(adcnum, clockpin, mosipin, misopin, cspin):
-        if (adcnum > 7) or (adcnum < 0):
-                return -1
-        GPIO.output(cspin, True)
-
-        GPIO.output(clockpin, False)  # start clock low
-        GPIO.output(cspin, False)     # bring CS low
-
-        commandout = adcnum
-        commandout |= 0x18  # start bit + single-ended bit
-        commandout <<= 3    # we only need to send 5 bits here
-        for i in range(5):
-                if (commandout & 0x80):
-                        GPIO.output(mosipin, True)
-                else:
-                        GPIO.output(mosipin, False)
-                commandout <<= 1
-                GPIO.output(clockpin, True)
-                GPIO.output(clockpin, False)
-
-        adcout = 0
-        # read in one empty bit, one null bit and 10 ADC bits
-        for i in range(12):
-                GPIO.output(clockpin, True)
-                GPIO.output(clockpin, False)
-                adcout <<= 1
-                if (GPIO.input(misopin)):
-                        adcout |= 0x1
-
-        GPIO.output(cspin, True)
-
-        adcout >>= 1       # first bit is 'null' so drop it
-        return adcout
 
 # Номера портов, которые будут использованы для SPI. Сюда будет подключен АЦП
 # SPI port on the ADC to the Cobbler
@@ -93,7 +60,7 @@ while True:
 
         # Считываем очередное значение с АЦП
         # read the analog pin
-        trim_pot = readadc(potentiometer_adc, SPICLK, SPIMOSI, SPIMISO, SPICS)
+        trim_pot = adc.readadc(potentiometer_adc, SPICLK, SPIMOSI, SPIMISO, SPICS)
 
         # Вычисляем разницу между текущим и прошлым значением
         # how much has it changed since the last read?
