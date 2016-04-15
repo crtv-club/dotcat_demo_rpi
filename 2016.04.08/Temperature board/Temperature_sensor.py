732bdeba826038f51ce03f503346e7321efc3262
Sergey Kostyuk <ks.hot.ua@gmail.com>
2016-04-15 16:47:55 +0300

    temp test

--- 2016.04.08/Temperature board/Temperature_sensor.py
+++ 2016.04.08/Temperature board/Temperature_sensor.py
@@ -1,11 +1,19 @@
+import time
+
 def OpenAndReadFile( _file ):
     TempOpenFile = open( _file )
-    tempText = TempOpenFile.read()
+    file = TempOpenFile.read()
     TempOpenFile.close()
-    return tempText
+    return file
+
 
 def SplitDataForTemperatureSensor( _file ):
     TempData = OpenAndReadFile( _file )
     SplitData = TempData.split("\n")[1].split(" ")[9]
     temperature = float(SplitData[2:])/1000
     return temperature
+
+while(True):
+    print(SplitDataForTemperatureSensor('/sys/bus/w1/devices/28-000003d38d8c/w1_slave'))
+    time.sleep(1)
+
