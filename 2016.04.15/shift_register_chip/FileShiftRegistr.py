f84a0f54cc7b97167800356629f976286709597b
staskond <staskond@gmail.com>
2016-04-16 16:20:53 +0300

    Проверить

--- 2016.04.15/shift_register_chip/FileShiftRegistr.py
+++ 2016.04.15/shift_register_chip/FileShiftRegistr.py
@@ -1,56 +1,65 @@
 import RPi.GPIO as GPIO
 
 
-#sh_cp - сдвиг
-#st_cp - синхро сигнал
-#data_in - входные данные(1 или 0)
-class ShiftRegistr(object):
-    def __init__(self, data_in, sh_cp, st_cp):#обьявляем конструктор
-        if type(data_in) != int or type(sh_cp) != int or type(st_cp):#проверка на правильность входных значений
+# sh_cp - LATCH- сдвиг
+# st_cp - CLK- синхро сигнал
+# data_in - входные данные(1 или 0)
+class ShiftRegister(object):
+    def __init__(self, data_in, LATCH, CLK):  # обьявляем конструктор
+        if type(data_in) != int or type(LATCH) != int or type(CLK) != int:  # проверка на правильность входных значений
             raise ValueError('Both arguments must be integers')
 
-        self.data_in = data_in#инициализация переменной
-        self.sh_cp = sh_cp#инициализация переменной
-        self.st_cp = st_cp#инициализация переменной
+        self.data_in = data_in  # инициализация переменной
+        self.LATCH = LATCH  # инициализация переменной
+        self.CLK = CLK  # инициализация переменной
 
-        GPIO.setup(self.data_in, GPIO.OUT)#Установка пина подачи данных на выход
-        GPIO.setup(self.sh_cp, GPIO.OUT)#Установка пина сдвига на выход
-        GPIO.setup(self.st_cp, GPIO.OUT)#Установка пина синхросигнала на выход
+        GPIO.setup(self.data_in, GPIO.OUT)  # Установка пина подачи данных на выход
+        GPIO.setup(self.LATCH, GPIO.OUT)  # Установка пина сдвига на выход
+        GPIO.setup(self.CLK, GPIO.OUT)  # Установка пина синхросигнала на выход
 
-    def __del__(self):#деструктор
-        self.stop()#подаем на все пины нули
-        GPIO.cleanup()#очищаем порты
+    def __del__(self):  # деструктор
+        self.stop()  # подаем на все пины нули
+        GPIO.cleanup()  # очищаем порты
 
-    def stop(self):#Функция остановки
-        GPIO.output(self.data_in, GPIO.LOW)#Устанавливаем пин в логический ноль
-        GPIO.output(self.sh_cp, GPIO.LOW)#Устанавливаем пин в логический ноль
-        GPIO.output(self.st_cp, GPIO.LOW)#Устанавливаем пин в логический ноль
+    def stop(self):  # Функция остановки
+        GPIO.output(self.data_in, GPIO.LOW)  # Устанавливаем пин в логический ноль
+        GPIO.output(self.LATCH, GPIO.LOW)  # Устанавливаем пин в логический ноль
+        GPIO.output(self.CLK, GPIO.LOW)  # Устанавливаем пин в логический ноль
+        return
 
-    def clk_puls(self):#Делаем скачек импульса подавая сначала 0, потом 1, потом снова 0
-        #для подачи сигнала что нам нужно сделать сдвиг
-        GPIO.output(self.sh_cp, GPIO.LOW)#подаем на пин логичиеский 0
-        GPIO.output(self.sh_cp, GPIO.HIGT)#подаем на пин логичискую 1
-        GPIO.output(self.sh_cp, GPIO.LOW)#подаем на пин логичиеский 0
+    def clk_puls(self):  # Делаем скачек импульса подавая сначала 0, потом 1, потом снова 0
+        # для подачи сигнала что нам нужно сделать сдвиг
+        GPIO.output(self.CLK, GPIO.HIGH)  # подаем на пин логичискую 1
+        GPIO.output(self.CLK, GPIO.LOW)  # подаем на пин логичиеский 0
+        return
 
-    def shift_reg(self, data):#Функция работы сдвига регистра
-        for i in range(7):#Задаем диапазон от 0 до 7(по количеству выходов на нашем микроконтроллере, напомню, их 8)
-            data_storage = -1# присваеваем -1, в случае ошибки записи в data_storage мы будем знать где ошибка
-            if data & 0x80:#Проверяем контролирующий бит
-                data_storage = 1#если он равен 1, записываем его в переменную и выходи из if-а
-            else:#в противном случае переходим дальше, и присваеваем переменной data_torage ноль
-                data_storage = 0
-            data = data_storage << 1#Сдвигаем регистр на 1 влево и записываем в переменную, цикл начинаеться заново
-            self.clk_puls()#даем импульс
-        return data#возвращаем переменную
+    def serLatch(self):
+        GPIO.output(self.LATCH, GPIO.HIGH)
+        # time.sleep(.01)
+        GPIO.output(self.LATCH, GPIO.LOW)
+        return
 
-    def send_reg(self, data):#функция отправки данных на регистр
-        GPIO.output(self.data_in, data)#отправляем данные
-        self.synchr()#Передаем скачек сигналов, тем самым давая понять что мы все передали
+    def shift_reg(self, data):  # Функция работы сдвига регистра
+        self.digital_write(GPIO.LOW)
+        for i in range(0, 8):  # Задаем диапазон от 0 до 7(по количеству выходов на нашем микроконтроллере, напомню, их 8)
+            temp = data & 0x80
+            if temp & 0x80:  # Проверяем контролирующий бит
+                GPIO.output(self.data_in, 1)  # если он равен 1, записываем его в переменную и выходи из if-а
+            else:  # в противном случае переходим дальше, и присваеваем переменной data_torage ноль
+                GPIO.output(self.data_in, 0)
+            self.clk_puls()
+            data = data << 1  # Сдвигаем регистр на 1 влево и записываем в переменную, цикл начинаеться заново
+        self.serLatch()
+        return
 
-    def synchr(self):#подаем сигнал синхроизациии(0 - 1 - 0)
-        GPIO.output(self.st_cp, GPIO.LOW)#Подаем логический 0
-        GPIO.output(self.st_cp, GPIO.HIGT)#Подаем логическую 1
-        GPIO.output(self.st_cp, GPIO.LOW)#Подаем логический 0
-
-
+    def convBinary(self, value):
+        binaryValue = '0b'
+        for x in range(0, 8):
+            temp = value & 0x80
+            if temp == 0x80:
+                binaryValue = binaryValue + '1'
+            else:
+                binaryValue = binaryValue + '0'
+            value = value << 1
+        return binaryValue
 
--- 2016.04.15/shift_register_chip/shift_register_motor.py
+++ 2016.04.15/shift_register_chip/shift_register_motor.py
@@ -1,20 +1,17 @@
-from FileShiftRegistr import ShiftRegistr
 import RPi.GPIO as GPIO
-#устанавливаем пины
-data_in = 3# пин для входных данных
-st_cp = 5#пин для синхросигнала
-sh_cp = 7#пин для сдвига
+from FileShiftRegistr import ShiftRegistr
 
-Data = 0x80# восьмибитное значения
-#которое будем менять для работы определенных моторов/диодов
-GPIO.setmode(GPIO.BOARD) #устанавливаем режим пинов
+# устанавливаем пины
+data_in = 3  # пин для входных данных
+st_cp = 5  # пин для синхросигнала
+sh_cp = 7  # пин для сдвига
 
+Data = 0b10011100 #
+# которое будем менять для работы определенных моторов/диодов
+GPIO.setmode(GPIO.BOARD)  # устанавливаем режим пинов
 
-RegistrForMotor = ShiftRegistr(data_in, sh_cp, st_cp)#конструируем объект регистра RegistrForMotor
+RegistrForDiods = ShiftRegistr(data_in, sh_cp, st_cp)
 
 while True:
-    DataForSendToController = RegistrForMotor.shift_reg(Data)#записываем значения
-    #которое должны подать в DataForSendToController
-    RegistrForMotor.send_reg(DataForSendToController)#отправляем значения на контроллер
-
-
+ temp = RegistrForDiods.converBinary(Data)
+ RegistrForDiods.shift_reg(temp)
