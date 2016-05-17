86db35768a95c31b8c2f217b3bf795fa0483823b
Sergey Kostyuk <ks.hot.ua@gmail.com>
2016-05-17 17:26:26 +0300

    Added code sample form multi-threading

---
+++ 2016.05.17/multi_threading.py
@@ -0,0 +1,16 @@
+# copy-pasted from: http://stackoverflow.com/questions/2905965/creating-threads-in-python
+
+from threading import Thread
+from time import sleep
+
+
+def threaded_function(arg):
+    for i in range(arg):
+        print("running")
+        sleep(1)
+
+if __name__ == "__main__":
+    thread = Thread(target = threaded_function, args = (10, ))
+    thread.start()
+    thread.join()
+    print("thread finished...exiting")
