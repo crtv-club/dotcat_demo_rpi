2bb026bc15293d5f571f41767774f3e0ae7284db
Sergey Kostyuk <ks.hot.ua@gmail.com>
2017-07-05 12:35:51 +0300

    Moved mqtt broker port to connection_settings

--- 2017.03.18/mqtt_temp_sensor_esp8266/connection_settings.h
+++ 2017.03.18/mqtt_temp_sensor_esp8266/connection_settings.h
@@ -1,5 +1,6 @@
 // Update these with values suitable for your network.
 
-const char* ssid = ".......";
-const char* password = ".......";
-const char* mqtt_server = ".......";
+const char*  ssid = ".......";
+const char*  password = ".......";
+const char*  mqtt_server = ".......";
+const int8_t mqtt_port = 1883;
